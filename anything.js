d3f39d3b151aee3a3e3c7d7ad46a1c640ce2c8aa
Lowrance Rawung <LowRainSir1509@Lowrances-MacBook-Pro.local>
2018-07-31 18:02:10 +0700

    bg chg

---
+++ anything.js
@@ -0,0 +1,24 @@
+/*
+var a = 3
+var b = 2
+var c = a + b
+console.log(c)
+*/
+
+var b = 10
+var g = 20
+
+function a() {
+  var f = 10
+  var g = 20
+  var t = f + g
+  console.log(t+b)
+}
+
+function b() {
+  var aa = 9
+  var c = 5
+  console.log(aa+c)
+}
+
+a();
